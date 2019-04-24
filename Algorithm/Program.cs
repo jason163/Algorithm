087d26a0818b43c1b763494f803ed98a98843332
jason <jason@JASON-PC>
2019-04-24 15:05:10 +0800

    algorithm for dynamic programming

---
+++ Algorithm/Program.cs
@@ -0,0 +1,48 @@
+﻿using Algorithm;
+using System;
+using System.Diagnostics;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursion recursion = new Recursion();
+
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            int sum = recursion.MaxSum(1,0);
+            Console.WriteLine($"The Max Value:{sum},used {sw.Elapsed.TotalMilliseconds} MilliSec by MaxSum()");
+            Console.WriteLine($"-----------------------------------------------------------------------------");
+            sw.Stop();
+
+            sw.Reset();
+            sw.Start();
+            int sumV1 = recursion.MaxSumV1(1,0);
+            Console.WriteLine($"The Max Value:{sumV1},used {sw.Elapsed.TotalMilliseconds} MilliSec by MaxSumV1");
+            Console.WriteLine($"-----------------------------------------------------------------------------");
+            sw.Stop();
+
+            sw.Reset();
+            sw.Start();
+            int sum1 = recursion.MaxSumWithCache();
+            Console.WriteLine($"The Max Value:{sum1},used {sw.Elapsed.TotalMilliseconds} MilliSec by MaxSumWithCache");
+            sw.Stop();
+            
+
+            Process curProcess = Process.GetCurrentProcess();
+            string cpu = ((Double)(curProcess.TotalProcessorTime.TotalMilliseconds -
+                curProcess.UserProcessorTime.TotalMilliseconds)).ToString();
+            string memory = (curProcess.WorkingSet64 / 1024 / 1024).ToString() + 
+                "M (" + (curProcess.WorkingSet64 / 1024).ToString() + "KB)";
+
+            Console.WriteLine($"Process ID:{curProcess.Id.ToString()} CPU:{cpu} Memory:{memory}");
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Algorithm/src/Recursion.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm
+{
+    /// <summary>
+    /// 递归算法
+    /// </summary>
+    public class Recursion
+    {
+        // 二维数组存储初始化数据
+        int[][] D = new int[6][];
+        int[][] tempArray = new int[6][];
+
+        int num = 5;
+        // 临时最大值
+        int[] tempMax = new int[5];
+        //
+        
+
+        public Recursion()
+        {
+            D[1] = new int[] { 7 };
+            D[2] = new int[] { 3, 8 };
+            D[3] = new int[] { 8, 1, 0 };
+            D[4] = new int[] { 2, 7, 4, 4 };
+            D[5] = new int[] { 4, 5, 2, 6, 5 };
+            // 初始值为边界值
+            tempMax = D[5];
+            for (int a = num ; a >= 1; --a)
+            {
+                tempArray[a] = new int[a];
+                for (int b = 0; b < a; ++b)
+                {
+                    tempArray[a][b] = -1;
+                }
+
+            }
+                
+        }
+
+        /// <summary>
+        /// 在上面的数字三角形中寻找一条从顶部到底边的路径，使得路径上所经过的数字之和最大。
+        /// 路径上的每一步都只能往左下或 右下走。只需要求出这个最大和即可，不必给出具体路径
+        /// </summary>
+        public int Start()
+        {
+            return MaxSum(1, 0);
+            
+        }
+        public int StartWithCache()
+        {
+            return MaxSumWithCache();
+        }
+        
+        /// <summary>
+        /// 求最大路径值
+        /// </summary>
+        /// <param name="i">行</param>
+        /// <param name="j">列</param>
+        /// <returns></returns>
+        public int MaxSum(int i,int j)
+        {
+
+            if (i == num)
+                return D[i][j];
+            int x = MaxSum(i + 1, j);
+            int y = MaxSum(i + 1, j + 1);
+            
+            int max =  Math.Max(x, y) + D[i][j];
+
+            Console.WriteLine($"Curr Row: {i} Column: {j},The Max:{max}");
+            return max;
+        }
+
+        public int MaxSumV1(int i, int j)
+        {
+            if(tempArray[i][j] != -1)
+            {
+                return tempArray[i][j];
+            }
+            if (i == num)
+                return D[i][j];
+            int x = MaxSumV1(i + 1, j);
+            int y = MaxSumV1(i + 1, j + 1);
+
+            tempArray[i][j] = Math.Max(x, y) + D[i][j]; 
+            Console.WriteLine($"Curr Row: {i} Column: {j},The Max:{tempArray[i][j]}");
+            return tempArray[i][j];
+        }
+
+        public int MaxSumWithCache()
+        {
+            for(int a=num-1;a>=1;--a)
+                for(int b = 0; b < a; ++b)
+                {
+                    tempMax[b] = Math.Max(tempMax[b], tempMax[b + 1]) + D[a][b];
+                }
+            return tempMax[0];
+        }
+    }
+}
